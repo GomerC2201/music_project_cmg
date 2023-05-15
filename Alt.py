1f57c19bf1335ac5bf14ed038afa14a106d7dc7c
GomerC2201 <78840139+GomerC2201@users.noreply.github.com>
2023-05-15 18:12:06 -0400

    Update Alt.py

--- Alt.py
+++ Alt.py
@@ -457,7 +457,7 @@ elif app_mode == "Bar Chart Race":
                 plot_bgcolor='rgb(211, 211, 211)',  # Make chart's background grey
                 showlegend=False,
                 height=600,
-                width=800,
+                width=820,
                 bargap=0.15,
                 bargroupgap=0.1,
                 yaxis={'categoryorder': 'total ascending'},
@@ -485,7 +485,7 @@ elif app_mode == "Bar Chart Race":
         yaxis_title="Artist",
         showlegend=False,
         height=600,
-        width=800,
+        width=820,
         bargap=0.15,
         bargroupgap=0.1,
         plot_bgcolor='rgb(211, 211, 211)',  # Make chart's background grey
